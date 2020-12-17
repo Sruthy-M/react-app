1bdfcc1ffc338bdfa953433d03daa07cebce8169
Sruthy-M <sruthy.manithody@gmail.com>
2020-12-17 17:36:11 +0530

    profile image uploaded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 //components
-import MyNavbar from "./components/my-navbar/mynavbar.component";
-import './App.css';
+import MyNavbar from "./components/my-navbar/my.navbar.components";
+import profile from './assets/img/profile/Profile-img.png';
 
+import './App.css';
+import Typical from 'react-typical'
  const App = () => {
-  return (<div>
-    <MyNavbar />
-    My Portfolio</div>);
+  return (
+     
   
-};
+  <div className='App'>
+    <MyNavbar />
+  <header className='App-header'>
+    <img src={profile} className='App-profile' alt='profile'/>
+  
+    <h1>Hi, I'm Sruthy Muraleedharan</h1>
+    <p> I'm a{' '}
+      <Typical
+         loop={Infinity}
+         wrappper='b'
+         steps={[
+           'developer ',
+           1000,
+          'designer',
+          1000,
+          'Engineering student',
+          1000,
+          'Photographer',
+          1000
+         ]}/>
+    </p>
+    </header>
+    </div>
+  );
+  
+}
+
 
 export default App;
