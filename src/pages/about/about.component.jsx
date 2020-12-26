aa76c327ae57c6fca7dc872002f48d13d2094c7f
Sruthy-M <sruthy.manithody@gmail.com>
2020-12-27 00:14:49 +0530

    some files are edited

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,18 +1,63 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/Profile-img.png";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 
 const About = () => {
-    return (
- 
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Sruthy Muraleedharan</strong>
+                <br />A passionate programmer and doing Engineering graduation in Computer Science, born and brought up in India. I am a Full Stack Web Developer with React.js, Express.js, Node.js, and as my tech stack.
+                <br />
+                    my goal is always driven towards providing amazing experience with the best level of quality and service to them.
+                <br />
+               I love learning about new technologies, what problems are they solving and How can I use them to build better and scalable products.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/Sruthy-M" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/sruthy-muraleedharan-ab18a619a/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
- <div id="about">
-     <div className=" align-items-start p-2 my-details rounded">
-     <u><b><h1 className="pt-3 text-center font-details pb-3">About Me</h1></b></u>
-          <p>Hi there! I am <strong>&nbsp;Sruthy Muraleedharan</strong><br />
-           A passionate programmer born and brought up in india,I am a Frontend web developer with React.js,Express.js,Node.js as my main stack,
-           I am studying Btech CS degree in Kerala Technical University.I love learning about emerging technologies,along with that i also want to be a professional dancer and i work for it.</p>        
-        </div>
-        </div>
-    )
-}
-
-export default About
+export default About;
